1c2ac5be7de254b908163043ad2b8ae2b389b2b2
honzuejtt-ops <honzue.jtt@gmail.com>
2026-04-01 18:30:00 +0200

    Add files via upload

---
+++ Arduino/HryKviz.h
@@ -0,0 +1,34 @@
+#ifndef HRY_KVIZ_H
+#define HRY_KVIZ_H
+
+#include <Arduino.h>
+
+const int kvizPocet = 10;
+
+const char* kvizOtazky[] = {
+  "Ktera planeta slunecni soustavy je nejblize Slunci?",
+  "Kdo napsal hru Romeo a Julie?",
+  "Jak se jmenuje nejvyssi hora sveta?",
+  "V jakem roce skoncila druha svetova valka?",
+  "Ktery prvek ma chemickou znacku O?",
+  "Jaka je hlavni mena v Japonsku?",
+  "Kdo namaloval obraz Mona Lisa?",
+  "Jak se nazyva nas kontinent?",
+  "Ktery ocean je nejvetsi?",
+  "Kolik barev ma duha?"
+};
+
+const char* kvizOdpovedi[] = {
+  "Merkur",
+  "William Shakespeare",
+  "Mount Everest",
+  "1945",
+  "Kyslik",
+  "Jen",
+  "Leonardo da Vinci",
+  "Evropa",
+  "Tichy ocean",
+  "Sedm"
+};
+
+#endif
